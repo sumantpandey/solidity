e0c8d42c73b9be67ae8d88b50260e456bb4c8f3b
sumant pandey <ssumant110059@gmail.com>
2022-07-28 18:48:54 +0530

    added new contract to illustrate use of internal visibility

---
+++ InternalVisibility.sol
@@ -0,0 +1,30 @@
+/*internal is the default visibility for state variables.
+Internal functions and state variables can both be accessed from within the same contract 
+and in deriving contracts. They aren't accessible from the outside.
+*/
+contract InternalVisibility{
+
+    uint256 internal myVar;
+    constructor(){
+        myVar = 10;
+    }
+    function myFunc() public view returns (uint256) {
+        uint256 myVarCopy = myVar; 
+    }
+    function getMyVar() internal view returns(uint){
+       return myVar;
+    }
+
+    function call() public view returns(uint){
+        return getMyVar();
+    }
+}
+    /**
+     below code is also fine, internal variable and methods can
+     be used in derived contract
+    */
+    contract DerivedContract is InternalVisibility {
+    function myFunc() public view returns (uint256) {
+        uint256 myVarCopy = myVar;
+    }
+    }
